37c801d6bb8e0c1549abda488f37d6a5f97f45ad
Gabriel Renaud gabriel.renaud@clin.au.dk <gabrielr@fe1.genomedk.net>
2019-10-02 17:18:02 +0200

    almost got predict done

--- cinch.py
+++ cinch.py
@@ -124,7 +124,7 @@ def which(program):
 
 mininsize=1;
 maxinsize=700;
-
+wcomponents=2;
 
 #print(pathofconfig);
 parser = OptionParser(
@@ -146,11 +146,13 @@ parser = OptionParser(
 "There are 2 modes:\n"+
 "\n"+
 "\tcynch train [fof]\n"+
-"\tcynch predict [fo]\n"
+    "\tcynch predict [model] [fof]\n"
 "\n"+
     "\tThe fof (file of file) as the following format for \"training\"\n"+
     "\t\t[bamfile1] [ctDNA frac 1]\n"+
     "\t\t[bamfile2] [ctDNA frac 2]\n"+
+    "\t\t...\n"+
+
     "\tFor example:\n"+
     "\t\t/data/path/in1.bam 0.59\n"+
     "\t\t/data/path/in2.bam 0.11\n"+
@@ -159,7 +161,17 @@ parser = OptionParser(
 "\tThe fof has the following format for \"predict\"\n"+
     "\t\t[bamfile1]\n"+
     "\t\t[bamfile2]\n"+
-    "\t\t"+
+    "\t\t..."+
+"\n"+
+"\n"+
+
+"\tThe [model] is created by the training and has the following shape\n"+
+    "\t\tcomp1_1 comp1_2 comp1_3 ... \n"+
+    "\t\tcomp2_1 comp2_2 comp2_3 ...\n"+
+    "\t\t..."+
+"\n"+
+"\n"+
+
 "\tThe program will auto-resume if you have sent commands to the cluster or used parallel\n"+
     
 "\n"
@@ -272,11 +284,7 @@ pathoffofnof   = "/".join(pathoffofarray);
 
 
 # train
-
-if(args[0] == "train"):
-    #####################################
-    #   stage 1: feature extraction     #
-    #####################################
+def readFOFlabeled(foffile):
     foffilefd = open(foffile, "r");
     bamfiles  = [];
     label     = [];
@@ -326,7 +334,175 @@ if(args[0] == "train"):
     foffilesub=re.sub('/','_',foffile);
     foffilesub=re.sub("\ ",'_',foffilesub);
     foffilesub=re.sub("\.",'_',foffilesub);
+
+    if(len(bamfiles) == 0):
+        sys.stderr.write("\nNo BAM files were found in "+str(foffile)+"\n");
+        sys.exit(1);
+
     
+    return bamfiles,label,foffilesub;
+
+def readFOFunlabeled(foffile):
+
+    foffilefd = open(foffile, "r");
+    bamfiles  = [];
+    
+    for linefd in foffilefd:
+        linefd = linefd.strip();
+        fields = linefd.split();
+
+        if(len(linefd)==0):
+            continue;
+
+        if(len(fields)!=1):
+            sys.stderr.write("\nThe line ->"+str(linefd)+"<- does not have 1 column, please check the expected format using -h\n");
+            sys.exit(1);
+       
+        bamfile = os.path.abspath(fields[0]);
+        #print(bamfile);
+        if(not os.path.exists(bamfile)):            
+            bamfile2 = os.path.abspath(pathoffofnof+"/"+fields[0]);
+            if(not os.path.exists(bamfile2)):            
+                sys.stderr.write("\nERROR: The BAM file "+bamfile+" or "+bamfile2+" does not exist, make sure you have the correct relative or absolute path\n");
+                sys.exit(1);
+            else:
+                if(not os.path.exists(bamfile2+".bai")):            
+                    sys.stderr.write("\nERROR: The BAM file "+bamfile2+" is not indexed, please index the bam files and rerun\n");
+                    sys.exit(1);
+                    
+                bamfiles.append( bamfile2 );
+        else:
+            if(not os.path.exists(bamfile+".bai")):            
+                sys.stderr.write("\nERROR: The BAM file "+bamfile+" is not indexed, please index the bam files and rerun\n");
+                sys.exit(1);
+
+            bamfiles.append( bamfile   );
+            
+        
+    foffilesub=re.sub('/','_',foffile);
+    foffilesub=re.sub("\ ",'_',foffilesub);
+    foffilesub=re.sub("\.",'_',foffilesub);
+
+    if(len(bamfiles) == 0):
+        sys.stderr.write("\nNo BAM files were found in "+str(foffile)+"\n");
+        sys.exit(1);
+
+
+    return bamfiles,foffilesub;
+
+
+def runStage1(resultso,threads,winsize,foffilesub,bamfiles,logfile):
+
+    sys.stderr.write("\n");
+    sys.stderr.write("    #####################################    \n");
+    sys.stderr.write("    #   stage 1: feature extraction     #    \n");
+    sys.stderr.write("    #####################################    \n");
+    sys.stderr.write("\n");
+
+    #CNV
+    if not os.path.exists( ""+resultso+"/stage1/"):
+        os.mkdir( ""+resultso+"/stage1/", 0755 );
+    else:
+        sys.stderr.write("\nThe directory "+resultso+"/stage1/"+" already exists\n");            
+
+    fileHandleLC = open ( ""+resultso+"/listcommands_1.txt", 'w' ) ;
+    #isize
+
+    #sys.stderr.write("bamfiles"+str(bamfiles));
+
+    for bami in range(0,len(bamfiles)):
+        fileHandleLC.write(pathofinsize+" "+bamfiles[bami]+" |sort -n |uniq -c |gzip > "+resultso+"/stage1/"+str(bami)+".isize.gz\n");
+
+    #readcount, todo reenable?
+    if False:
+        for bami in range(0,len(bamfiles)):
+            fileHandleLC.write(pathofreadcounter+" -w "+str(winsize)+" -c "+str( ",".join(chrarray) )+" "+bamfiles[bami]+"  > "+resultso+"/stage1/"+str(bami)+".seg\n");
+    fileHandleLC.close();
+
+    #logfile = (resultso+foffilesub+"_train.log");
+    logfilefp = open(logfile, "w");
+    logfilefp.write("#-o:"+resultso+"\n");
+    logfilefp.write("#fof:"+foffile+"\n");
+    logfilefp.write("#stage1:\n");
+    logfilefp.close();
+
+    print("Please run the commands manually either using:");
+    print("  cat "+resultso+"/listcommands_1.txt | parallel -j "+str(threads));
+    print("on the use a batch/queueing system to launch:");
+    print("  cat "+resultso+"/listcommands_1.txt | sbatch ...");
+    print("");
+    print("Once commands are done, rerun with:\n");
+    print("  cinch.py   -o "+resultso+"  train "+foffile);
+    print("");
+
+    cmdtolaunch="cat "+resultso+"/listcommands_1.txt | parallel  -j "+str(threads);
+    
+def parseIsize(resultso,foffilesub,bamfiles):
+    isizedat = (resultso+foffilesub+"_isize.dat");
+    isizedatfp = open(isizedat, "w");
+
+    isizedatfp.write("#fileidx");
+    for i in range(mininsize,(maxinsize+1)):
+        isizedatfp.write( "\t"+str( i  ) );
+    isizedatfp.write( "\n" );
+    sys.stderr.write("Reading bam files\n");
+
+    #dataAllisize = np.array([])
+    dataAllisize = [];
+
+    for bami in tqdm(range(0,len(bamfiles))):
+        fileisize = resultso+"/stage1/"+str(bami)+".isize.gz";
+        #datatoadd = np.array([]);
+        datatoadd = [];
+
+        if(not os.path.exists(fileisize)):
+            sys.stderr.write("\nThe file "+fileisize+" does not exist, please run all commands.\n");
+            sys.exit(1);
+        else:
+            isizeCount=[];
+            for i in range(0,maxinsize+1):
+                isizeCount.append(0);
+            #print(fileisize);
+            fileisizefd = gzip.open(fileisize, "r");
+
+            for lineisfd in fileisizefd:
+                #add filters and store
+                fields=lineisfd.split( );
+
+                try:
+                    count   = int(fields[0]);
+                    isize   = int(fields[1]);
+
+                except ValueError:
+                    continue;
+
+                if( (isize<mininsize) or (isize>maxinsize) ):
+                    continue;
+
+                isizeCount[ isize ] = count;
+
+            isizedatfp.write(str(bami));
+            for i in range(mininsize,(maxinsize+1)):
+                isizedatfp.write( "\t"+str( isizeCount[ i ]) );
+                datatoadd.append( isizeCount[ i ]  );
+
+            isizedatfp.write( "\n" );
+
+            dataAllisize.append( datatoadd );
+
+    isizedatfp.close();
+    sys.stderr.write("\nWriten isize data to "+str(resultso+foffilesub+"_isize.dat")+".\n");
+    return dataAllisize;
+
+if(args[0] == "train"):
+
+    #####################################
+    #   stage 1: feature extraction     #
+    #####################################
+
+    bamfiles,label,foffilesub  =  readFOFlabeled(foffile);
+    
+
     logfile = (resultso+foffilesub+"_train.log");
     print(logfile);
 
@@ -339,49 +515,7 @@ if(args[0] == "train"):
         #read file of file
         
         #insert size
-        sys.stderr.write("\n");
-        sys.stderr.write("    #####################################    \n");
-        sys.stderr.write("    #   stage 1: feature extraction     #    \n");
-        sys.stderr.write("    #####################################    \n");
-        sys.stderr.write("\n");
-
-
-        #CNV
-        if not os.path.exists( ""+options.resultso+"/stage1/"):
-            os.mkdir( ""+options.resultso+"/stage1/", 0755 );
-        else:
-            sys.stderr.write("\nThe directory "+options.resultso+"/stage1/"+" already exists\n");            
-
-
-        fileHandleLC = open ( ""+options.resultso+"/listcommands_1.txt", 'w' ) ;
-        #isize
-        for bami in range(0,len(bamfiles)):
-            fileHandleLC.write(pathofinsize+" "+bamfiles[bami]+" |sort -n |uniq -c |gzip > "+options.resultso+"/stage1/"+str(bami)+".isize.gz\n");
-
-        #readcount, todo reenable?
-        if False:
-            for bami in range(0,len(bamfiles)):
-                fileHandleLC.write(pathofreadcounter+" -w "+str(options.winsize)+" -c "+str( ",".join(chrarray) )+" "+bamfiles[bami]+"  > "+options.resultso+"/stage1/"+str(bami)+".seg\n");
-        fileHandleLC.close();
-        
-        
-        logfilefp = open(logfile, "w");
-        logfilefp.write("#-o:"+options.resultso+"\n");
-        logfilefp.write("#fof:"+foffile+"\n");
-        logfilefp.write("#stage1:\n");
-        logfilefp.close();
-
-        print("Please run the commands manually either using:");
-        print("  cat "+options.resultso+"/listcommands_1.txt | parallel -j "+str(options.threads));
-        print("on the use a batch/queueing system to launch:");
-        print("  cat "+options.resultso+"/listcommands_1.txt | sbatch ...");
-        print("");
-        print("Once commands are done, rerun with:\n");
-        print("  cinch.py   -o "+options.resultso+"  train "+foffile);
-        print("");
-
-        cmdtolaunch="cat "+options.resultso+"/listcommands_1.txt | parallel  -j "+str(options.threads);
-        
+        runStage1(options.resultso,options.threads,options.winsize,foffilesub,bamfiles,logfile);
 
         
     else:
@@ -436,86 +570,12 @@ if(args[0] == "train"):
             #########################
             #      PARSE ISIZE      #
             #########################
-            isizedat = (resultso+foffilesub+"_isize.dat");
-            isizedatfp = open(isizedat, "w");
-
-
-            isizedatfp.write("#fileidx");
-            for i in range(mininsize,(maxinsize+1)):
-                isizedatfp.write( "\t"+str( i  ) );
-            isizedatfp.write( "\n" );
-            sys.stderr.write("Reading bam files\n");
-
-
-            #dataAllisize = np.array([])
-            dataAllisize = [];
-
-            for bami in tqdm(range(0,len(bamfiles))):
-                fileisize = options.resultso+"/stage1/"+str(bami)+".isize.gz";
-                #datatoadd = np.array([]);
-                datatoadd = [];
-
-                if(not os.path.exists(fileisize)):
-                    sys.stderr.write("\nThe file "+fileisize+" does not exist, please run all commands.\n");
-                    sys.exit(1);
-                else:
-                    isizeCount=[];
-                    for i in range(0,maxinsize+1):
-                        isizeCount.append(0);
-                    #print(fileisize);
-                    fileisizefd = gzip.open(fileisize, "r");
-
-                    for lineisfd in fileisizefd:
-                        #add filters and store
-                        fields=lineisfd.split( );
-                        
-                        try:
-                            count   = int(fields[0]);
-                            isize   = int(fields[1]);
-                            
-                        except ValueError:
-                            continue;
-                            
-                        if( (isize<mininsize) or (isize>maxinsize) ):
-                            continue;
-                        
-                            #print(idx);
-                        #print(isize);
-
-                        isizeCount[ isize ] = count;
-                        #print(fields[0]);
-                        #print(fields[1]);
-
-                    isizedatfp.write(str(bami));
-                    for i in range(mininsize,(maxinsize+1)):
-                        isizedatfp.write( "\t"+str( isizeCount[ i ]) );
-                        datatoadd.append( isizeCount[ i ]  );
-
-                    #print("datatoadd");
-                    #print(type(datatoadd));
-                    #print(datatoadd);
-
-                    #print("dataAllisize");
-                    #print( type( dataAllisize) );
-
-                    #print(dataAllisize.shape);
-                    #print(datatoadd.shape);
-                    isizedatfp.write( "\n" );
-                    #normalize
-                    #if( bami == 0 ):
-                    #    dataAllisize =   [datatoadd];
-                    #else:
-                    #dataAllisize = np.append( dataAllisize,  np.array(datatoadd)  , axis=0 );
-                    dataAllisize.append( datatoadd );
-                    
-            isizedatfp.close();
-            sys.stderr.write("\nWriten isize data to "+str(resultso+foffilesub+"_isize.dat")+".\n");
-
+            dataAllisize = parseIsize(options.resultso,foffilesub,bamfiles);
             #########################
             #      run HMMcopy      #
             #########################
             #normalize
-            print("dataAllisize");
+            #print("dataAllisize");
             #print(dataAllisize.shape);
             #print(dataAllisize);
 
@@ -523,24 +583,39 @@ if(args[0] == "train"):
             #dataAllisizeNorm  = dataAllisize / row_sums[:, numpy.newaxis]
             
             dataAllisizeNorm = np.array(normalize(dataAllisize,norm='l1'));
-            print(dataAllisize[0]);
-            print(dataAllisizeNorm[0]);
+            #print(dataAllisize[0]);
+            #print(dataAllisizeNorm[0]);
 
-            nmf = NMF(n_components=2, init='random', random_state=0)
+            #nmf = NMF(n_components=2, init='random', random_state=0)
             
 
-            W = nmf.fit_transform(dataAllisizeNorm);
-            H = nmf.components_;
-
+            #W = nmf.fit_transform(dataAllisizeNorm);
+            #H = nmf.components_;
+            sys.stderr.write("\nRunning NMF...");
+            W, H, n_iter = non_negative_factorization(dataAllisizeNorm, n_components=wcomponents,init='random', random_state=0,solver="mu",beta_loss=1,max_iter=200)
+            sys.stderr.write("done\n");
 
             print("W");
             print(W);
+            
+            hfile = (resultso+foffilesub+"_"+str(wcomponents)+"_H.dat");
+            hfilefp = open(hfile, "w");
+            
+            rows = H.shape[0]
+            cols = H.shape[1]
+            for i in range(0, rows):
+                hfilefp.write( str(H[i,0]) );
+                for j in range(1, cols):
+                    hfilefp.write( "\t"+str(H[i,j]) );
+                hfilefp.write( "\n" );
+            hfilefp.close();
+
 
             WNorm = np.array(normalize(W,norm='l1'));
             print(WNorm);
 
-
-
+            sys.stderr.write("\nWriten H matrix  "+hfile+".\n");
+            
 
 
 
@@ -566,9 +641,54 @@ if(args[0] == "predict"):
     #stage 1: feature extraction
     #stage 2: reading model +prediction
 
-    foffile=args[1];
+    hmatrix=args[1];
+    foffile=args[2];
 
+
+    #print(foffile);
+    bamfiles,foffilesub  =  readFOFunlabeled(foffile);
+    
+
+    logfile = (resultso+foffilesub+"_predict.log");
+    print(logfile);
+
+
+    #stage 2: training+writing model
+    
+    
+    if(not os.path.exists(logfile)):#step 1
+        stage=1;
+        #read file of file
+        
+        #insert size
+        runStage1(options.resultso,options.threads,options.winsize,foffilesub,bamfiles,logfile);
+    else:
+        dataAllisize = parseIsize(options.resultso,foffilesub,bamfiles);
+        print(dataAllisize);
+        dataAllisizeNorm = np.array(normalize(dataAllisize,norm='l1'));
+
+        hfile = hmatrix;
+        hfilefp = open(hfile, "r");
+        Hmat=[];
+        for hline in hfilefp:
+            fields=hline.split( );
+            Hmattoadd=[];
+
+            for i in range(0,len(fields)):
+                #print(fields[i]);
+                Hmattoadd.append( float(fields[i]) );
+            
+            Hmat.append( Hmattoadd );
+
+        Hnp = np.array( Hmat );
+        print(Hnp.shape);
+        print(dataAllisizeNorm.shape);
+
+        W, H, n_iter = non_negative_factorization(dataAllisizeNorm, n_components=wcomponents,W=None, H=Hnp,init='custom', random_state=0,solver="mu",beta_loss=1,max_iter=200)
+        print(W);
+    print(hmatrix);
     print(foffile);
+
     sys.exit(0);
 
 
